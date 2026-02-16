0952af7970241d7138409913ba61b8998611b571
Andrej Koller <andrejkoller@outlook.com>
2026-02-16 19:36:56 +0100

    feat: implement Header component and integrate it into ClientLayout

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -1,9 +1,14 @@
+import { Header } from "@/components/header";
+
 export default function ClientLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <main className="flex flex-col min-h-screen text-sm">
-      <div className="max-w-175 w-full mx-auto px-4 grow">{children}</div>
-    </main>
+    <div className="flex flex-col min-h-screen text-sm">
+      <div className="max-w-175 w-full mx-auto px-4 grow">
+        <Header />
+        <main>{children}</main>
+      </div>
+    </div>
   );
 }
---
+++ components/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+  return (
+    <header className="h-50 flex items-center fade-in">
+      <div className="flex flex-col items-start">
+        {/* SVG Logo coming soon */}
+      </div>
+    </header>
+  );
+};
