0cb2caf0b4c5aba9e8572a83e8f15ec83c041db7
Andrej Koller <andrejkoller@outlook.com>
2026-02-23 14:18:40 +0100

    feat: update ClientLayout and Home components with improved structure and styling

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -7,7 +7,9 @@ export default function ClientLayout({
   return (
     <div className="flex flex-col min-h-screen text-sm">
       <Header />
-      <main className="max-w-175 w-full mx-auto px-4 grow">{children}</main>
+      <main className="max-w-175 w-full min-h-screen mx-auto px-4 grow">
+        {children}
+      </main>
       <Footer />
     </div>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,19 @@
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black"></div>
+    <>
+      <section className="mb-12 fade-in delay-1">
+        <p className="mb-4 text-[#666]">
+          <span className="text-white">Andrej Koller</span> is a software
+          engineer from Ekibastuz, Kazakhstan.
+        </p>
+        <p className="mb-8 text-[#666]">Based in Passau, Germany.</p>
+      </section>
+      <section></section>
+      <section></section>
+      <div>
+        <section></section>
+        <section></section>
+      </div>
+    </>
   );
 }
