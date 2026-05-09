e6206fdf9088277a78f9460c4149c2fb59f80e72
Andrej Koller <andrejkoller@outlook.com>
2026-05-09 12:41:55 +0200

    Refactor project links in Home component to use configuration file

---
+++ configs/project-links-config.ts
@@ -0,0 +1,18 @@
+export const projectLinksConfig = [
+  {
+    name: "Athanasa",
+    url: "https://linktr.ee/athanasa",
+  },
+  {
+    name: "Fading Hell",
+    url: "https://linktr.ee/fadinghell",
+  },
+  {
+    name: "Digital Carnival",
+    url: "https://linktr.ee/digitalcarnival",
+  },
+  {
+    name: "Open Presenter",
+    url: "https://linktr.ee/openpresenter",
+  },
+];
