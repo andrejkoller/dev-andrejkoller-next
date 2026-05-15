d981113ed2d41e655ef767a6ae651ecf2f0f6c1e
Andrej Koller <andrejkoller@outlook.com>
2026-05-15 18:32:47 +0200

    Refactor project links to use Next.js Link component and add isExternal property for external links

--- configs/project-links-config.ts
+++ configs/project-links-config.ts
@@ -3,15 +3,18 @@ export const projectLinksConfig = [
     key: "athanasa",
     name: "Athanasa",
     url: "https://linktr.ee/athanasa",
+    isExternal: true,
   },
   {
     key: "fadinghell",
     name: "Fading Hell",
     url: "https://linktr.ee/fadinghell",
+    isExternal: true,
   },
   {
     key: "digitalcarnival",
     name: "Digital Carnival",
     url: "https://linktr.ee/digitalcarnival",
+    isExternal: true,
   },
 ];
