239140d589842fdb959c46179f55208daa78c511
Andrej Koller <andrejkoller@outlook.com>
2026-05-17 20:41:33 +0200

    Refactor footer links to use configuration file and update border styles in components

---
+++ configs/footer-links-config.ts
@@ -0,0 +1,22 @@
+export const footerLinksConfig = [
+  {
+    key: "x",
+    name: "𝕏",
+    href: "https://x.com/andrejkoller",
+  },
+  {
+    key: "dribbble",
+    name: "Dribbble",
+    href: "https://dribbble.com/andrejkoller",
+  },
+  {
+    key: "github",
+    name: "GitHub",
+    href: "https://github.com/andrejkoller",
+  },
+  {
+    key: "linkedin",
+    name: "LinkedIn",
+    href: "https://www.linkedin.com/in/andrejkoller",
+  },
+];
