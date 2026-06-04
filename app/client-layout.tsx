e93975e7be1ecda9d1d6a52b2b718515b8228cde
Andrej Koller <andrejkoller@outlook.com>
2026-06-04 18:26:04 +0200

    feat: implement theme context and provider for theme management

--- app/client-layout.tsx
+++ app/client-layout.tsx
@@ -2,21 +2,25 @@
 
 import { Footer } from "@/components/footer";
 import { Header } from "@/components/header";
+import { ThemeProvider } from "@/components/theme/theme-provider";
 
 export default function ClientLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="flex flex-col min-h-screen text-sm">
-      {/* Header */}
-      <Header />
+    <ThemeProvider>
+      <div className="flex flex-col min-h-screen text-sm">
+        {/* Header */}
+        <Header />
 
-      <main className="max-w-2xl w-full min-h-screen mx-auto px-4 grow">
-        {children}
-      </main>
+        {/* Main content */}
+        <main className="max-w-2xl w-full min-h-screen mx-auto px-4 grow">
+          {children}
+        </main>
 
-      {/* Footer */}
-      <Footer />
-    </div>
+        {/* Footer */}
+        <Footer />
+      </div>
+    </ThemeProvider>
   );
 }
